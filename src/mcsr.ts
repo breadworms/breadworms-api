e68097d1c9631678614d12dd07e18de109cfd00f
breadworms <breadwormsbusiness@gmail.com>
2026-04-08 04:39:09 +0000

    Add session record to elo command

--- src/mcsr.ts
+++ src/mcsr.ts
@@ -11,16 +11,57 @@ const userEmotes: Record<string, string> = {
   'breadworms': 'bert'
 };
 
+async function get(path: string): Promise<{ status: 'error' | 'success', data: any } | null> {
+  try {
+    return await fetch(`https://api.mcsrranked.com/${path}`).then(res => res.json());
+  } catch {
+    return null;
+  }
+}
+
 export async function getUserElo(identifier: string) {
-  const userData = await fetch(`https://api.mcsrranked.com/users/${encodeURIComponent(identifier)}`)
-    .then(res => res.json());
-  const elo: number = userData?.data?.eloRate ?? -1;
+  const res = await get(`users/${encodeURIComponent(identifier)}`);
+
+  if (!res || (res.status === 'error' && res.data?.error !== 'User is not exists.')) {
+    return `error fetching data`;
+  } else if (!res.data?.eloRate) {
+    return `not rated`;
+  }
+
+  const { uuid, eloRate } = res.data;
+  const matchesRes = await get(`users/${uuid}/matches?count=100&type=2`);
+  const sessionText = printSessionForUser(uuid, matchesRes?.data ?? []);
 
   for (const [threshold, bracket] of eloBrackets) {
-    if (elo >= threshold) {
-      return `${bracket} ${elo} ${userEmotes[identifier] ?? 'ep'}`;
+    if (eloRate >= threshold) {
+      return `${bracket} ${eloRate} ${userEmotes[identifier] ?? ''}${sessionText}`;
     }
   }
 
-  return `not rated`;
+  throw new Error(`Could not print elo: ${identifier}'s elo was ${eloRate} (a negative number).`);
+}
+
+function printSessionForUser(uuid: string, matches: any[]) {
+  const terminal = Math.round(Date.now() * 0.001) - 60 * 60 * 12;
+  let wins = 0;
+  let losses = 0;
+  let draws = 0;
+
+  for (const match of matches) {
+    if (match.date <= terminal) {
+      break;
+    }
+
+    if (match.result.uuid === uuid) {
+      wins += 1;
+    } else if (match.result.uuid !== null) {
+      losses += 1;
+    } else {
+      draws += 1;
+    }
+  }
+
+  return wins + losses + draws > 0
+    ? ` (today: ${wins}-${losses}-${draws})`
+    : '';
 }
